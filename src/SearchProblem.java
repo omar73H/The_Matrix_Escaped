530b019fbdd66a356ac5ff9cd92ae7faf9613aa8
Omar Alaskndrani <omar.alaskndrani.1@gmail.com>
2021-10-29 22:57:58 +0200

    Goal test implemented

--- src/SearchProblem.java
+++ src/SearchProblem.java
@@ -1,7 +1,28 @@
 
 public class SearchProblem {
-	byte numOperators = 8; // or enum
+	byte numOperators = 8; // or array of enum
 	State initialState;
 	// State space!?
 	
+	short numOfHostages;
+	
+	public SearchProblem(byte numOfHostages,State initialState) {
+		this.numOfHostages=numOfHostages;
+		 this.initialState= initialState;
+		
+	}
+	
+	public boolean goalTest(State state) {
+		// Check all hostages are either delivered or died, All converted hostages are killed
+		
+		// No one in the currently carried
+		if(state.currentlyCarriedHostages!=0)
+			return false;
+		
+		// hostages in movedHostages + killed hostagesAgent == no. of all hostages
+		if((state.movedHostages |state.killedTransHostages)!= (1<<numOfHostages)-1)
+			return false;
+		
+		return true;
+	}
 }
