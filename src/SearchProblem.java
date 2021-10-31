3510c86c26db9ec3b6a2cc45d0a32dbdfaa3308e
Eiad21 <eiadessam1@gmail.com>
2021-10-31 20:03:10 +0200

    Added method to calculate pathcost

--- src/SearchProblem.java
+++ src/SearchProblem.java
@@ -1,10 +1,34 @@
-
 public class SearchProblem {
 	byte numOperators = 8; // or array of enum
 	State initialState;
 	// State space!?
 	
 	short numOfHostages;
+
+	public static int calculatePathCost(Node node) {
+		// OPERATOR: Move(0) : 2, Kill(1): 2 if mutated and 20 otherwise, Carry(2) : 2, Drop(3): -500, Fly(4): 2, Take_Pill(5): 2
+		// STATE: Neo Dies: 10000, Hostage Dies 1000
+		byte op = node.operator;
+		int baseCost = 0;
+		switch(op) {
+		case(0): baseCost = 2;break;
+		case(1): baseCost = 20;break; // how to check if mutated
+		case(2): baseCost = 2;break;
+		case(3): baseCost = -500;break;
+		case(4): baseCost = 2;break;
+		case(5): baseCost = 2;break;
+		}
+		if(node.state.neoHealth<=0) {
+			baseCost+=10000;
+		}
+		int prevHostages=0;
+		int currHostages=0;
+		for( int x : node.parent.state.hostagesHealth) if(x==0) prevHostages++;
+		for( int x : node.state.hostagesHealth) if(x==0) currHostages++;
+		
+		baseCost+=(currHostages-prevHostages)*1000;
+		return (node.parent != null)? baseCost + node.parent.pathCost :0 ;
+	}
 	
 	public SearchProblem(byte numOfHostages,State initialState) {
 		this.numOfHostages=numOfHostages;
@@ -24,5 +48,25 @@ public class SearchProblem {
 			return false;
 		
 		return true;
+	}
+	
+	public static void main(String[] args) {
+		byte neoX=0;
+		byte neoY=0;
+		short movedHostages=0;
+		short currentlyCarriedHostages=0;
+		short hostagesToAgents=0;
+		short killedTransHostages=0;
+		byte[]  prehostagesHealth= {10,5,6,7,0};
+		byte[]  currhostagesHealth= {10,5,6,11,0};
+
+		byte neoHealth=0;
+		State prevState=new State(neoX,neoY,movedHostages,currentlyCarriedHostages,hostagesToAgents,killedTransHostages,prehostagesHealth,(byte)50);
+		State currState=new State(neoX,neoY,movedHostages,currentlyCarriedHostages,hostagesToAgents,killedTransHostages,currhostagesHealth,neoHealth);
+//		(State state, Node parent, byte operator, short depth, short pathCost)
+		Node parent=new Node(prevState,null,(byte) 1,(short) 10,(short) 100);
+		Node node=new Node(currState,parent,(byte) 1,(short) 10,(short) 100);
+		System.out.println(calculatePathCost(node));
+
 	}
 }
